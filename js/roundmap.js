0a4dd524c58d9f11e66575b1b289bb717fdaab5e
martilidad <martilidad@users.noreply.github.com>
2020-05-06 23:48:15 +0200

    get guesses now responds with list

--- js/roundmap.js
+++ js/roundmap.js
@@ -43,7 +43,7 @@ function rminitialize() {
 };
 
 function renderOtherGuesses() {
-  var map = window.map
+  var map = window.map;
   //intermediate static object before I figure out how to get the data
   $.ajax({
       url: "http://" + window.location.host + "/guess",
@@ -52,19 +52,17 @@ function renderOtherGuesses() {
         "Location_ID": window.loc['Location_ID'],
       },
       success: function (otherGuesses) {
-        for (var name in otherGuesses) {
-          if (otherGuesses.hasOwnProperty(name)) {
-            var guess = otherGuesses[name];
+          for (let i = 0; i < otherGuesses.length; i++) {
+            var guess = otherGuesses[i];
             var ltLng = new google.maps.LatLng(guess['Lat'], guess['Long']);
             var Marker = new google.maps.Marker({
               position: ltLng,
-              label: name,
+              label: guess['Name'],
               icon: 'img/other.png',
               title: 'Distance: ' + guess['Distance'] + ' Score: ' + guess['Score']
             });
             Marker.setMap(map);
           }
-        }
       },
       error: function (result) {
         console.log(result);
